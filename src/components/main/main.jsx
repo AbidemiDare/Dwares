c9cb8bc1629552159e82a731d702f8f5d4c73468
AbidemiDare <dreysblog1@gmail.com>
2024-08-26 12:29:01 +0100

    Updated the main

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -6,48 +6,23 @@ import "./main.css";
 function Main() {
   const buttons = ["All", "Phones", "Laptops", "Others"];
 
-  const [moveBtn, setMoveBtn] = useState(0);
-  const [minClick, setMinClick] = useState(0)
-  const [click, setClick] = useState(0)
-  const addElement = useRef(null);
+  // const [moveBtn, setMoveBtn] = useState(0);
+  // const addElement = useRef(null);
 
-  const minClicks = 3;
-  const maxClicks = 3;
 
-   const handleMinClicks = () => {
-    if(minClick < minClicks) {
-      setMinClick(minClick + 1)
-    }
-  }
+  // const moveRight = () => {
+  //   setMoveBtn(moveBtn + 50);
+  // };
 
-  const moveRight = () => {
-    setMoveBtn(moveBtn + 50);
-  };
+  // const moveLeft = () => {
+  //   setMoveBtn(moveBtn - 50);
 
-  const combineRight = () => {
-    moveRight()
-    handleMinClicks()
-  }
+  //   const hasClass = addElement.current.classList.contains("show");
+  //   if (!hasClass) {
+  //     addElement.current.classList.add("show");
+  //   }
+  // };
 
-   const handleClicks = () => {
-    if(click < maxClicks) {
-      setClick(click + 1)
-    } 
-  }
-
-  const moveLeft = () => {
-    setMoveBtn(moveBtn - 50);
-
-    const hasClass = addElement.current.classList.contains("show");
-    if (!hasClass) {
-      addElement.current.classList.add("show");
-    }
-  };
-
-  const handleMaxClicks = () => {
-    moveLeft()
-    handleClicks()
-  }
 
   return (
     <div>
@@ -57,21 +32,21 @@ function Main() {
             <div className="products-btn-container">
               <div className="products-btns">
                 {buttons.map((name, id) => (
-                  <Productbtns id={id} btn={name} moveBtn={moveBtn} />
+                  <Productbtns id={id} btn={name} />
                 ))}
               </div>
 
-              <button
-                className={`scroll-btn right ${minClick === minClicks ? 'hidden' : ' '}`}
+              {/* <button
+                className="scroll-btn right"
                 ref={addElement}
-                onClick={combineRight} disabled={minClick >= minClicks}>
+                onClick={moveRight}>
                 {" "}
                 &#10095;
               </button>
 
-              <button className={`scroll-btn left ${click === maxClicks ? 'hidden' : 'show'}`} onClick={handleMaxClicks} disabled={click >= maxClicks}>
+              <button className="scroll-btn left" onClick={moveLeft}>
                 &#10094;
-              </button>
+              </button> */}
             </div>
 
             <Products />
--- src/components/main/products/productbtns.jsx
+++ src/components/main/products/productbtns.jsx
@@ -1,15 +1,21 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 function Productbtns({ id, btn, moveBtn }) {
   const [activeKey, setActiveKey] = useState(0);
+  const [activeIndex, setIndex] = useState(null)
+
+  const handleClick = (id) => {
+    setIndex(id)
+
+    if(activeIndex === id ) {
+      
+    }
+  }
 
   return (
     <div>
       {/* <div className="product-btn product-btn-one product-active">All</div> */}
-      <div
-        className={`product-btn ${id === activeKey ? "product-active" : " "}`}
-        style={{ transform: `translateX(${moveBtn}px)` }}
-      >
+      <div key={id} className={`product-btn ${id === activeKey ? "product-active" : " "}`} onClick={() => handleClick(id)}>
         {btn}
       </div>
     </div>
